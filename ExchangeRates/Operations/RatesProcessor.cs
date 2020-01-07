c3f7d26757b034e202eb68b1a5fbc6de91351864
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2020-01-07 16:50:48 +0100

    Create Layout - GUI

---
+++ ExchangeRates/Operations/RatesProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace ExchangeRates
+{
+    class RatesProcessor
+    {
+        public class DataObject
+        {
+            public string Name { get; set; }
+        }
+        public async Task<List<RateModel>> GetTodaysRates()
+        {
+            string url = $"http://api.nbp.pl/api/exchangerates/tables/A/today?format=json";
+           
+            //HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            using (HttpResponseMessage response = APIhelper.ApiClient.GetAsync(url).Result)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    //string json = new WebClient().DownloadString(url);
+                    //var ListOfRates = JsonConvert.DeserializeObject<List<RateModel>>(json);
+                   
+                    var rates = await response.Content.ReadAsAsync<List<RateModel>>();
+                    return rates;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+              
+            }
+        }
+    }
+}
